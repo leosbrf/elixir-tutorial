e1cc39f2d64f9ca58a843d61c68aace2c07f4d0b
FRANCISCO OLIVEIRA <leo.sbrf@gmail.com>
2020-02-09 01:05:56 -0300

    documentation in elixir

--- ch02/ex1-drop/lib/ex1-drop.ex
+++ ch02/ex1-drop/lib/ex1-drop.ex
@@ -3,20 +3,18 @@ defmodule Drop do
   Documentation for Drop.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Drop.hello()
-      :world
-
-  """
-
   # you can import globally here or inside the function scope
 
   # you also can import everything "except". i.e:
   # import :math, except: [sin: 1, cos: 1]
+
+  @doc """
+  Calculates the velocity of an object falling on Earth as if it
+  were in a vacuum (no air resistance).  The distance is the
+  height from which the object falls, specified in meters, and
+  the function returns a velocity in meters per second.
+  """
+  @spec fall_velocity(number()) :: float()
 
   def fall_velocity(distance) do
     import :math, only: [sqrt: 1]
@@ -27,6 +25,15 @@ defmodule Drop do
     sqrt(2 * 9.8 * distance)
   end
 
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Drop.hello()
+      :world
+
+  """
   defp hello do
     :world
   end
